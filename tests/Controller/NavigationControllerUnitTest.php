0112275887f21a6a57f15c0ba3bd393d7c37566e
Clement <clementthuet7@gmail.com>
2020-03-27 09:52:49 +0100

    Added and fix some unit tests

---
+++ tests/Controller/NavigationControllerUnitTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class UserControllerUnitTest extends WebTestCase
+{
+    private $client = null;
+  
+    function setUp()
+    {
+       $this->client = static::createClient();
+    }
+    
+    public function loginAsAdmin()
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'Clement';
+        $form['_password'] = 'test';
+        $this->client->submit($form);
+    }
+    
+    public function testHomepageIsUp()
+    {
+      $this->client->request('GET', '/');
+      static::assertEquals(
+        200,
+        $this->client->getResponse()->getStatusCode()
+      );
+    }
+    
+    public function testLoginpageIsUp()
+    {
+        $this->client->request('GET', '/login');
+        static::assertEquals(
+          200,
+          $this->client->getResponse()->getStatusCode()
+        );
+    }
+    
+    //Test if there is a form with action = /login_check on the page at the URL "login"
+    public function testLoginFormIsUp()
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $this->assertSame(1, $crawler->filter('form[action=\/login_check]')->count());
+    }
+    
+    public function testLogin()
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'Clement';
+        $form['_password'] = 'test';
+       
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+        $this->assertTrue($crawler->filter('html:contains("Bienvenue sur Todo List")')->count() > 0);
+    }
+    
+    public function testUsersPageIsUp()
+    {
+        $this->loginAsAdmin();
+        $crawler = $this->client->request('GET', '/users');
+        $this->assertTrue($crawler->filter('html:contains("Liste des utilisateurs")')->count() > 0);
+    }
+    
+    public function testCreateUserPageIsUp()
+    {
+        $this->loginAsAdmin();
+        $crawler = $this->client->request('GET', '/users/create');
+        $this->assertTrue($crawler->filter('html:contains("Créer un utilisateur")')->count() > 0);
+    }
+    
+    public function testTasksPageIsUp()
+    {
+        $this->loginAsAdmin();
+        $crawler = $this->client->request('GET', '/tasks');
+        $this->assertTrue($crawler->filter('html:contains("Liste des tâches")')->count() > 0);
+    }
+    
+    public function testCreateTaskPageIsUp()
+    {
+        $this->loginAsAdmin();
+        $crawler = $this->client->request('GET', '/tasks/create');
+        $this->assertTrue($crawler->filter('html:contains("Créer une tâche")')->count() > 0);
+    }
+    
+    
+    
+    
+}
--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -13,4 +13,15 @@ class TaskTest extends TestCase{
         $this->assertEquals(true, is_a($task->getCreatedAt(),'DateTime'));
         $this->assertEquals(false, $task->isDone());
     }
+    
+    public function testUpdateTask()
+    {
+        $user = new Task();
+        $user->setTitle('Lorem Ipsum');
+        $user->setContent('Sit dolore');
+        $titleTask = $user->getTitle();
+        $contentTask = $user->getContent();
+        $this->assertEquals($titleTask, "Lorem Ipsum");
+        $this->assertEquals($contentTask, "Sit dolore");
+    }
 }
